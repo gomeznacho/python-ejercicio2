81efabedf8445a3198f96e116b86976afd4b7a77
Nacho Gomez <jimbo.greender@gmail.com>
2022-02-18 11:43:21 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+nombre = 'james'
+apellidos = 'jonez bonez'
+edad = 31
+email = 'jonez@bonez.com'
+casado = True
+con_hijos = False
+lista_de_amigos = ['Marty', 'George', 'Paul']
+peliculas_vistas= ['man from earth', 'otras peliculas']
+
+print(nombre,'\n', apellidos, '\n', edad, '\n', email, '\n',
+      casado, '\n', con_hijos, '\n', lista_de_amigos, '\n', peliculas_vistas)
